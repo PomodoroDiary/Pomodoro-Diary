132df9e85b41c6280bc9dee053a16fd1fb6a1ab7
b1y9mn <b1y9mn@gmail.com>
2023-05-15 09:38:02 +0900

    feat: 타이머 시작 기능 추가

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 const Timer = () => {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault(); 
+    setIsStarted(!isStarted);
+  }
+
   return (
     <>
-      <form>
-        <h1>15: 00</h1>
+      <form onSubmit={submitHandler}>
+        {!isStarted ? <h1>15: 00</h1> : <h1>14: 59</h1>}
         <button type="submit">시작</button>
       </form>
     </>
