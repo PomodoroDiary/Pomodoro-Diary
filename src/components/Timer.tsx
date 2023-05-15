69a1ea219ff9596f55602d088fefe028981f2e83
b1y9mn <b1y9mn@gmail.com>
2023-05-15 09:31:10 +0900

    feat: 타이머 시작 페이지 추가

---
+++ src/components/Timer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Timer = () => {
+  return (
+    <>
+      <form>
+        <h1>15: 00</h1>
+        <button type="submit">시작</button>
+      </form>
+    </>
+  );
+};
+
+export default Timer;
